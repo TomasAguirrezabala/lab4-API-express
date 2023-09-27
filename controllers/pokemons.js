f2e2a63343f68c77571e3e55fbfd48d7b3a0218c
Tomas Aguirrezabala <tomiaguirrezabala@gmail.com>
2023-09-27 01:37:44 -0300

    .

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -5,7 +5,7 @@ const getPokemons = async (req = request, res = response) => {
   try {
     const { limit = 50, offset = 0, api } = req.query;
     const miApi = process.env.API_KEY;
-    if (111 === miApi){
+    if (api === miApi){
       const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?api_key=${api}&limit=${limit}&offset=${offset}`);
       const pokemons = response.data.results;
       res.status(200).json(pokemons);
@@ -21,31 +21,40 @@ const getPokemons = async (req = request, res = response) => {
 const getPokemon = async (req = request, res = response) => {
   try {
     const { id } = req.params;
-    const api = process.env.API_KEY;
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}?api_key=${api}`);
-    const pokemon = response.data;
-    res.status(200).json(pokemon);
-  } catch (error) {
-    console.error(error);
-    res.status(404).json({ error: 'Pokémon no encontrado' });
-  }
+    const {api} = req.query;
+    const miApi = process.env.API_KEY;
+    if (api === miApi){
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}?api_key=${api}`);
+      const pokemon = response.data;
+      res.status(200).json(pokemon);
+    }else{
+      res.status(401).json({error: 'Unauthorized'})
+    }
+    } catch (error) {
+      console.error(error);
+      res.status(404).json({ error: 'Pokémon no encontrado' });
+    }
 };
 
 const getAbilitiesLista = async (req = request, res = response) => {
-  const {limit, offset } = req.query;
-  const api = process.env.API_KEY;
+  const {limit, offset, api } = req.query;
+  const miApi = process.env.API_KEY;
   let query_params = '';
   query_params += limit && `&limit=${limit}`;
   query_params += offset && `&offset=${offset}`;
   try {
-    const response = await axios.get(`https://pokeapi.co/api/v2/ability?api_key=${api}${query_params}`);
-    const abilities = response.data;
-    const results = abilities.results;
-    res.status(200).json(results);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error inesperado' });
-  }
+    if (api === miApi){    
+      const response = await axios.get(`https://pokeapi.co/api/v2/ability?api_key=${api}${query_params}`);
+      const abilities = response.data;
+      const results = abilities.results;
+      res.status(200).json(results);
+    }else{
+      res.status(401).json({error: 'Unauthorized'})
+    }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error inesperado' });
+    }
 };
 
 module.exports = {
