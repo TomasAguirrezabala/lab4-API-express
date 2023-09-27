9a28af36402e168218e2e9529bf1fb03c58e5a2b
Tomas Aguirrezabala <tomiaguirrezabala@gmail.com>
2023-09-27 00:46:15 -0300

    .

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -31,10 +31,10 @@ const getAbilitiesLista = async (req = request, res = response) => {
   const {limit, offset } = req.query;
   const api = process.env.API_KEY;
   let query_params = '';
-  query_params += limit && `limit=${limit}`;
+  query_params += limit && `$limit=${limit}`;
   query_params += offset && `&offset=${offset}`;
   try {
-    const response = await axios.get(`https://pokeapi.co/api/v2/ability/?api_key=${api}&${query_params}`);
+    const response = await axios.get(`https://pokeapi.co/api/v2/ability/?api_key=111${query_params}`);
     const abilities = response.data;
     const results = abilities.results;
     res.status(200).json(results);
